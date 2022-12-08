51e67e61fc12dba41651c9cf389410e6052c6a3e
Manuel Mena <manuelmena1993@gmail.com>
2022-12-08 00:01:24 -0300

    feat: names endpoint

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "@well-known-components/http-server"
 import { GlobalContext } from "../types"
+import { namesHandler } from "./handlers/names-handler"
 import { pingHandler } from "./handlers/ping-handler"
 import { profilesHandler } from "./handlers/profiles-handler"
 import { wearablesHandler } from "./handlers/wearables-handler"
@@ -11,6 +12,7 @@ export async function setupRouter(globalContext: GlobalContext): Promise<Router<
   router.get("/ping", pingHandler)
   router.post('/profiles', profilesHandler)
   router.get('/nfts/wearables/:id', wearablesHandler)
+  router.get('/nfts/names/:id', namesHandler)
 
   return router
 }
