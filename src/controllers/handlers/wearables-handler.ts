193b1763a214e762797ca634a4c65f39f81d72f0
Manuel Mena <manuelmena1993@gmail.com>
2022-12-16 00:44:10 -0300

    feat: add paginationToken to params

--- src/controllers/handlers/wearables-handler.ts
+++ src/controllers/handlers/wearables-handler.ts
@@ -6,6 +6,17 @@ export async function wearablesHandler(context: HandlerContextWithPath<'config' 
   const { id } = context.params
   const pageSize = context.url.searchParams.get('pageSize')
   const pageNum = context.url.searchParams.get('pageNum')
+  const paginationToken = context.url.searchParams.get('paginationToken')
+
+  // Error if token is not present when asking for a page > 1
+  if (pageNum && parseInt(pageNum) > 1 && !paginationToken) {
+    return {
+      status: 400,
+      body: {
+        error: "'paginationToken' must be provided when requesting a 'pageNum' greater than 1"
+      }
+    }
+  }
 
   // Get wearables for requested address
   const wearables = await getWearablesForAddress(context.components, id, pageSize, pageNum)
