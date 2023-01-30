5c2d6d7ed7d784c4e6718d3ebd3672b4f44ce637
Manuel Mena <manuelmena1993@gmail.com>
2023-01-30 10:56:32 -0300

    feat: add collectionId

--- src/controllers/handlers/wearables-handler.ts
+++ src/controllers/handlers/wearables-handler.ts
@@ -1,16 +1,24 @@
+import { getWearablesForCollection } from '../../logic/third-party-wearables'
 import { getWearablesForAddress } from '../../logic/wearables'
 import { HandlerContextWithPath } from '../../types'
 
 export async function wearablesHandler(context: HandlerContextWithPath<'config' | 'theGraph' | 'wearablesCaches' | 'fetch', '/nfts/wearables/:id'>) {
   // Get request params
   const { id } = context.params
+  const includeTPW = context.url.searchParams.has('includeThirdParty')
   const pageSize = context.url.searchParams.get('pageSize')
   const pageNum = context.url.searchParams.get('pageNum')
   const orderBy = context.url.searchParams.get('orderBy')
-  const includeTPW = context.url.searchParams.has('includeThirdParty')
+  const collectionId = context.url.searchParams.get('collectionId')
   
-  // Get wearables response
-  const wearablesResponse = await getWearablesForAddress(context.components, id, includeTPW, pageSize, pageNum, orderBy)
+  let wearablesResponse
+  if (collectionId) {
+    // If collectionId is present, only that collection thir-party wearables are sent
+    wearablesResponse = await getWearablesForCollection(context.components, collectionId, id)
+  } else {
+    // Get full cached wearables response
+    wearablesResponse = await getWearablesForAddress(context.components, id, includeTPW, pageSize, pageNum, orderBy)
+  }
 
   return {
     status: 200,
