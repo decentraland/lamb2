60da0ca73f5d42471f6985313ce166179da554e4
Pedro Tamborindeguy <pftambo@gmail.com>
2023-03-09 16:41:35 -0300

    chore: abstraction in definitions-fetcher

--- src/adapters/definitions-fetcher.ts
+++ src/adapters/definitions-fetcher.ts
@@ -2,6 +2,7 @@ import LRU from 'lru-cache'
 import { IBaseComponent } from '@well-known-components/interfaces'
 import { AppComponents, Definition } from '../types'
 import { extractEmoteDefinitionFromEntity, extractWearableDefinitionFromEntity } from './definitions'
+import { Entity } from '@dcl/schemas'
 
 export type DefinitionsFetcher = IBaseComponent & {
   fetchWearablesDefinitions(urns: string[]): Promise<(Definition | undefined)[]>
@@ -12,19 +13,22 @@ export async function createDefinitionsFetcherComponent({
   config,
   content
 }: Pick<AppComponents, 'logs' | 'config' | 'content'>): Promise<DefinitionsFetcher> {
-  const wearablesSize = (await config.getNumber('WEARABLES_CACHE_MAX_SIZE')) ?? 1000
-  const wearablesAge = (await config.getNumber('WEARABLES_CACHE_MAX_AGE')) ?? 600000 // 10 minutes by default
+  const itemsSize = (await config.getNumber('ITEMS_CACHE_MAX_SIZE')) ?? 1000
+  const itemsAge = (await config.getNumber('ITEMS_CACHE_MAX_AGE')) ?? 600000 // 10 minutes by default
 
   // TODO create lower case cache, get/set wrapped to set the key to lowercase
-  const wearablesDefinitionsCache = new LRU<string, Definition>({
-    max: wearablesSize,
-    ttl: wearablesAge
+  const itemDefinitionsCache = new LRU<string, Definition>({
+    max: itemsSize,
+    ttl: itemsAge
   })
 
-  async function fetchWearablesDefinitions(urns: string[]): Promise<(Definition | undefined)[]> {
+  async function fetchItemsDefinitions(
+    urns: string[],
+    mapEntityToDefinition: (components: Pick<AppComponents, 'content'>, entity: Entity) => Definition
+  ): Promise<(Definition | undefined)[]> {
     const nonCachedURNs: string[] = []
     for (const urn of urns) {
-      const definition = wearablesDefinitionsCache.get(urn.toLowerCase())
+      const definition = itemDefinitionsCache.get(urn.toLowerCase())
       if (!definition) {
         nonCachedURNs.push(urn)
       }
@@ -33,36 +37,16 @@ export async function createDefinitionsFetcherComponent({
     if (nonCachedURNs.length !== 0) {
       const entities = await content.fetchEntitiesByPointers(nonCachedURNs)
       for (const entity of entities) {
-        const definition = extractWearableDefinitionFromEntity({ content }, entity)
-        wearablesDefinitionsCache.set(definition.id.toLowerCase(), definition)
+        const definition = mapEntityToDefinition({ content }, entity)
+        itemDefinitionsCache.set(definition.id.toLowerCase(), definition)
       }
     }
 
-    return urns.map((urn) => wearablesDefinitionsCache.get(urn.toLowerCase()))
-  }
-
-  async function fetchEmotesDefinitions(urns: string[]): Promise<(Definition | undefined)[]> {
-    const nonCachedURNs: string[] = []
-    for (const urn of urns) {
-      const definition = wearablesDefinitionsCache.get(urn.toLowerCase())
-      if (!definition) {
-        nonCachedURNs.push(urn)
-      }
-    }
-
-    if (nonCachedURNs.length !== 0) {
-      const entities = await content.fetchEntitiesByPointers(nonCachedURNs)
-      for (const entity of entities) {
-        const definition = extractEmoteDefinitionFromEntity({ content }, entity)
-        wearablesDefinitionsCache.set(definition.id.toLowerCase(), definition)
-      }
-    }
-
-    return urns.map((urn) => wearablesDefinitionsCache.get(urn.toLowerCase()))
+    return urns.map((urn) => itemDefinitionsCache.get(urn.toLowerCase()))
   }
 
   return {
-    fetchWearablesDefinitions,
-    fetchEmotesDefinitions
+    fetchWearablesDefinitions: (urns) => fetchItemsDefinitions(urns, extractWearableDefinitionFromEntity),
+    fetchEmotesDefinitions: (urns) => fetchItemsDefinitions(urns, extractEmoteDefinitionFromEntity)
   }
 }
