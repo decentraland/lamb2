6aa1b18f177206288bee6a1a8ce5abc51a172142
Pedro Tamborindeguy <pftambo@gmail.com>
2023-03-10 14:34:21 -0300

    fix: tests

--- test/unit/ownership.spec.ts
+++ test/unit/ownership.spec.ts
@@ -1,16 +1,14 @@
-import { createConfigComponent, createDotEnvConfigComponent } from "@well-known-components/env-config-provider"
-import { createLogComponent } from "@well-known-components/logger"
-import { createTestMetricsComponent } from "@well-known-components/metrics"
+import { createDotEnvConfigComponent } from "@well-known-components/env-config-provider"
 import sinon from "sinon"
 import { ownedNFTsByAddress } from "../../src/logic/ownership"
-import { createTestTheGraphComponent } from "../components"
+import { createTheGraphComponentMock } from "../mocks/the-graph-mock"
 
 
 describe("ownership unit tests", () => {
   it("ownedNFTsByAddress must return an empty map when receives an empty map", async () => {
     const components = {
-      theGraph: createTestTheGraphComponent(),
-      config: await createDotEnvConfigComponent({}, {NFT_FRAGMENTS_PER_QUERY: '10'})
+      theGraph: createTheGraphComponentMock(),
+      config: await createDotEnvConfigComponent({}, { NFT_FRAGMENTS_PER_QUERY: '10' })
     }
     const nftIdsByAddressToCheck = new Map()
     const querySubgraph = sinon.stub()
@@ -21,8 +19,8 @@ describe("ownership unit tests", () => {
 
   it("ownedNFTsByAddress must return a map with the address but no owned nfts", async () => {
     const components = {
-      theGraph: createTestTheGraphComponent(),
-      config: await createDotEnvConfigComponent({}, {NFT_FRAGMENTS_PER_QUERY: '10'})
+      theGraph: createTheGraphComponentMock(),
+      config: await createDotEnvConfigComponent({}, { NFT_FRAGMENTS_PER_QUERY: '10' })
     }
     const nftIdsByAddressToCheck = new Map([
       ['address', ['nft']]
@@ -40,8 +38,8 @@ describe("ownership unit tests", () => {
 
   it("ownedNFTsByAddress must return a map with the addresses and their owned nfts", async () => {
     const components = {
-      theGraph: createTestTheGraphComponent(),
-      config: await createDotEnvConfigComponent({}, {NFT_FRAGMENTS_PER_QUERY: '10'})
+      theGraph: createTheGraphComponentMock(),
+      config: await createDotEnvConfigComponent({}, { NFT_FRAGMENTS_PER_QUERY: '10' })
     }
     const nftIdsByAddressToCheck = new Map([
       ['0x1', ['nft1', 'nft2']],
@@ -69,8 +67,8 @@ describe("ownership unit tests", () => {
 
   it("ownedNFTsByAddress must return a map with the addresses and some of the nfts not owned", async () => {
     const components = {
-      theGraph: createTestTheGraphComponent(),
-      config: await createDotEnvConfigComponent({}, {NFT_FRAGMENTS_PER_QUERY: '10'})
+      theGraph: createTheGraphComponentMock(),
+      config: await createDotEnvConfigComponent({}, { NFT_FRAGMENTS_PER_QUERY: '10' })
     }
     const nftIdsByAddressToCheck = new Map([
       ['0x1', ['nft1', 'nft2']],
@@ -98,8 +96,8 @@ describe("ownership unit tests", () => {
 
   it("ownedNFTsByAddress must return a map with the addresses and their owned nfts, with multiple subgraph calls", async () => {
     const components = {
-      theGraph: createTestTheGraphComponent(),
-      config: await createDotEnvConfigComponent({}, {NFT_FRAGMENTS_PER_QUERY: '1'})
+      theGraph: createTheGraphComponentMock(),
+      config: await createDotEnvConfigComponent({}, { NFT_FRAGMENTS_PER_QUERY: '1' })
     }
     const nftIdsByAddressToCheck = new Map([
       ['0x1', ['nft1', 'nft2']],
@@ -141,15 +139,15 @@ describe("ownership unit tests", () => {
 
   it('ownedNFTsByAddress must consider the nfts as owned if the query to subgraph fails', async () => {
     const components = {
-      theGraph: createTestTheGraphComponent(),
-      config: await createDotEnvConfigComponent({}, {NFT_FRAGMENTS_PER_QUERY: '10'})
+      theGraph: createTheGraphComponentMock(),
+      config: await createDotEnvConfigComponent({}, { NFT_FRAGMENTS_PER_QUERY: '10' })
     }
     const nftIdsByAddressToCheck = new Map([
       ['0x1', ['nft1', 'nft2']],
       ['0x2', ['nft1', 'nft2']]
     ])
     const querySubgraph = sinon.stub()
-    querySubgraph.withArgs(components.theGraph, [['0x1', ['nft1', 'nft2']],['0x2', ['nft1', 'nft2']]]).resolves([
+    querySubgraph.withArgs(components.theGraph, [['0x1', ['nft1', 'nft2']], ['0x2', ['nft1', 'nft2']]]).resolves([
       {
         owner: '0x2',
         ownedNFTs: ['nft1']
