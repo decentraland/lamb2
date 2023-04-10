2a0d4291bceedbcdd66c295094bda261a6dcb9c1
Hugo Arregui <hugo.arregui@mykolab.com>
2023-04-10 14:43:54 -0300

    chore: remove .env, use alpine, don't generate images on pr (#138)

--- test/unit/status-controller.spec.ts
+++ test/unit/status-controller.spec.ts
@@ -1,42 +1,45 @@
-import { createConfigComponent, createDotEnvConfigComponent } from "@well-known-components/env-config-provider"
-import { createTestMetricsComponent } from "@well-known-components/metrics"
-import { statusHandler } from "../../src/controllers/handlers/status-handler"
-import { metricDeclarations } from "../../src/metrics"
+import { createConfigComponent, createDotEnvConfigComponent } from '@well-known-components/env-config-provider'
+import { createTestMetricsComponent } from '@well-known-components/metrics'
+import { statusHandler } from '../../src/controllers/handlers/status-handler'
+import { metricDeclarations } from '../../src/metrics'
 
-describe("status-controller-unit", () => {
-  it("must return commit hash", async () => {
-    const url = new URL("https://github.com/well-known-components")
-    const config = await createDotEnvConfigComponent({}, {COMMIT_HASH: 'commit_hash'})
+describe('status-controller-unit', () => {
+  it('must return commit hash', async () => {
+    const url = new URL('https://github.com/well-known-components')
+    const config = await createDotEnvConfigComponent({}, { COMMIT_HASH: 'commit_hash' })
     expect(await statusHandler({ url, components: { config } })).toMatchObject({ body: { commitHash: 'commit_hash' } })
   })
 
-  it("must return current version", async () => {
-    const url = new URL("https://github.com/well-known-components")
-    const config = await createDotEnvConfigComponent({}, {CURRENT_VERSION: 'current_version'})
+  it('must return current version', async () => {
+    const url = new URL('https://github.com/well-known-components')
+    const config = await createDotEnvConfigComponent({}, { CURRENT_VERSION: 'current_version' })
     expect(await statusHandler({ url, components: { config } })).toMatchObject({ body: { version: 'current_version' } })
   })
 
-  it("must return default content server address", async () => {
-    const url = new URL("https://github.com/well-known-components")
-    const config = await createDotEnvConfigComponent({}, {})
-    expect(await statusHandler({ url, components: { config } })).toMatchObject({ body: { contentServerUrl: 'https://peer.decentraland.org/content' } })
+  it('must return default content server address', async () => {
+    const url = new URL('https://github.com/well-known-components')
+    const config = await createDotEnvConfigComponent(
+      {},
+      { CONTENT_SERVER_ADDRESS: 'https://peer.decentraland.org/content' }
+    )
+    expect(await statusHandler({ url, components: { config } })).toMatchObject({
+      body: { contentServerUrl: 'https://peer.decentraland.org/content' }
+    })
   })
 
-  it("must return default content server address", async () => {
-    const url = new URL("https://github.com/well-known-components")
+  it('must return currentTime', async () => {
+    const url = new URL('https://github.com/well-known-components')
     const config = await createDotEnvConfigComponent({}, {})
-    expect(await statusHandler({ url, components: { config } })).toMatchObject({ body: { contentServerUrl: 'https://peer.decentraland.org/content' } })
+    expect(await statusHandler({ url, components: { config } })).toMatchObject({
+      body: { currentTime: expect.any(Number) }
+    })
   })
 
-  it("must return currentTime", async () => {
-    const url = new URL("https://github.com/well-known-components")
+  it('must return empty for values that does not have default value', async () => {
+    const url = new URL('https://github.com/well-known-components')
     const config = await createDotEnvConfigComponent({}, {})
-    expect(await statusHandler({ url, components: { config } })).toMatchObject({ body: { currentTime: expect.any(Number) } })
-  })
-
-  it("must return empty for values that does not have default value", async () => {
-    const url = new URL("https://github.com/well-known-components")
-    const config = await createDotEnvConfigComponent({}, {})
-    expect(await statusHandler({ url, components: { config } })).toMatchObject({ body: { commitHash: '', version: '' } })
+    expect(await statusHandler({ url, components: { config } })).toMatchObject({
+      body: { commitHash: '', version: '' }
+    })
   })
 })
