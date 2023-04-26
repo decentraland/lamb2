f486d7955a18c98255151933adb66fce29b1b5cc
Pedro Tamborindeguy <pftambo@gmail.com>
2023-04-26 19:51:37 -0300

    chore: comment test

--- test/integration/explorer-handler.spec.ts
+++ test/integration/explorer-handler.spec.ts
@@ -4,9 +4,7 @@ import { WearableFromQuery } from '../../src/logic/fetch-elements/fetch-items'
 import { ContentComponent } from '../../src/ports/content'
 import { testWithComponents } from '../components'
 import {
-  generateThirdPartyWearables,
   generateWearableContentDefinitions,
-  generateWearables,
   getThirdPartyProviders
 } from '../data/wearables'
 
@@ -42,55 +40,55 @@ testWithComponents(() => {
     })
   })
 
-  it.only('return base + on-chain + third-party wearables', async () => {
-    const { content, fetch, localFetch, theGraph } = components
-    const onChainWearables = generateWearables(10)
-    const thirdPartyWearables = generateThirdPartyWearables(2)
-    // jest.spyOn(baseItems, 'BASE_WEARABLES').mockReturnValue(['urn:decentraland:off-chain:base-avatars:BaseFemale', 'urn:decentraland:off-chain:base-avatars:BaseMale'])
-    // Object.defineProperty(baseItems, 'BASE_WEARABLES', [])
-    // const baseWearableIds = ['baseWearable1', 'baseWearable2']
-    // const baseWearables = generateBaseWearables(BASE_WEARABLES)
+  // it('return base + on-chain + third-party wearables', async () => {
+  //   const { content, fetch, localFetch, theGraph } = components
+  //   const onChainWearables = generateWearables(10)
+  //   const thirdPartyWearables = generateThirdPartyWearables(2)
+  //   // jest.spyOn(baseItems, 'BASE_WEARABLES').mockReturnValue(['urn:decentraland:off-chain:base-avatars:BaseFemale', 'urn:decentraland:off-chain:base-avatars:BaseMale'])
+  //   // Object.defineProperty(baseItems, 'BASE_WEARABLES', [])
+  //   // const baseWearableIds = ['baseWearable1', 'baseWearable2']
+  //   // const baseWearables = generateBaseWearables(BASE_WEARABLES)
 
-    // const baseWearableDefinitions = baseWearables.map((wearable) => wearable.definition)
+  //   // const baseWearableDefinitions = baseWearables.map((wearable) => wearable.definition)
 
-    // const definitions = generateWearableContentDefinitions(
-    //   BASE_WEARABLES.concat(onChainWearables.map((wearable) => wearable.urn)).concat(
-    //     thirdPartyWearables.map((wearable) => wearable.urn.decentraland)
-    //   )
-    // )
+  //   // const definitions = generateWearableContentDefinitions(
+  //   //   BASE_WEARABLES.concat(onChainWearables.map((wearable) => wearable.urn)).concat(
+  //   //     thirdPartyWearables.map((wearable) => wearable.urn.decentraland)
+  //   //   )
+  //   // )
 
-    // const baseWearableDefinitions = generateWearableContentDefinitions(
-    // content.fetchEntitiesByPointers = jest.fn().mockResolvedValueOnce(definitions)
-    content.fetchEntitiesByPointers = jest.fn(async (pointers) => {
-      return generateWearableContentDefinitions(pointers)
-    })
-    content.getExternalContentServerUrl = jest.fn().mockReturnValue('contentUrl')
-    theGraph.ethereumCollectionsSubgraph.query = jest.fn().mockResolvedValueOnce({ nfts: onChainWearables.slice(0, 5) })
-    theGraph.maticCollectionsSubgraph.query = jest.fn().mockResolvedValueOnce({ nfts: onChainWearables.slice(5, 10) })
-    fetch.fetch = jest.fn().mockImplementation((url) => {
-      if (url.includes('babydoge')) {
-        return {
-          ok: true,
-          json: () => ({
-            assets: thirdPartyWearables
-          })
-        }
-      } else {
-        return { ok: true, json: () => ({ assets: [] }) }
-      }
-    })
+  //   // const baseWearableDefinitions = generateWearableContentDefinitions(
+  //   // content.fetchEntitiesByPointers = jest.fn().mockResolvedValueOnce(definitions)
+  //   content.fetchEntitiesByPointers = jest.fn(async (pointers) => {
+  //     return generateWearableContentDefinitions(pointers)
+  //   })
+  //   content.getExternalContentServerUrl = jest.fn().mockReturnValue('contentUrl')
+  //   theGraph.ethereumCollectionsSubgraph.query = jest.fn().mockResolvedValueOnce({ nfts: onChainWearables.slice(0, 5) })
+  //   theGraph.maticCollectionsSubgraph.query = jest.fn().mockResolvedValueOnce({ nfts: onChainWearables.slice(5, 10) })
+  //   fetch.fetch = jest.fn().mockImplementation((url) => {
+  //     if (url.includes('babydoge')) {
+  //       return {
+  //         ok: true,
+  //         json: () => ({
+  //           assets: thirdPartyWearables
+  //         })
+  //       }
+  //     } else {
+  //       return { ok: true, json: () => ({ assets: [] }) }
+  //     }
+  //   })
 
-    const r = await localFetch.fetch(`/explorer-service/backpack/${Wallet.generate().getAddressString()}/wearables`)
+  //   const r = await localFetch.fetch(`/explorer-service/backpack/${Wallet.generate().getAddressString()}/wearables`)
 
-    expect(r.status).toBe(200)
-    // const body = await r.json()
-    // expect(body).toEqual({
-    //   elements: [...convertToMixedOnChainWearable(onChainWearables), ...convertToMixedOnChainWearable(onChainWearables).filter(hasRarity).sort(rarest)],
-    //   pageNum: 1,
-    //   pageSize: 100,
-    //   totalAmount: BASE_WEARABLES.length + onChainWearables.length + thirdPartyWearables.length
-    // })
-  })
+  //   expect(r.status).toBe(200)
+  //   // const body = await r.json()
+  //   // expect(body).toEqual({
+  //   //   elements: [...convertToMixedOnChainWearable(onChainWearables), ...convertToMixedOnChainWearable(onChainWearables).filter(hasRarity).sort(rarest)],
+  //   //   pageNum: 1,
+  //   //   pageSize: 100,
+  //   //   totalAmount: BASE_WEARABLES.length + onChainWearables.length + thirdPartyWearables.length
+  //   // })
+  // })
 })
 
 function hasRarity<T>(item: T & { rarity: string }): item is T & { rarity: string } {
