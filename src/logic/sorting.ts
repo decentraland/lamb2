8f3b08e9400772f86aa285283ef6cf48a073bcb6
Mariano Goldman <mariano.goldman@decentraland.org>
2023-04-27 12:33:14 -0300

    new lint rule

--- src/logic/sorting.ts
+++ src/logic/sorting.ts
@@ -1,6 +1,8 @@
 import { RARITIES } from './utils'
 
-const byUrn: SortingFunction<HasUrn> = (item1: HasUrn, item2: HasUrn): number => item1.urn.localeCompare(item2.urn)
+function byUrn(item1: HasUrn, item2: HasUrn): number {
+  return item1.urn.localeCompare(item2.urn)
+}
 
 export type HasUrn = { urn: string }
 export type HasName = { name: string } & HasUrn
@@ -13,27 +15,27 @@ function compareByRarity(item1: HasRarity, item2: HasRarity) {
   return w2RarityValue - w1RarityValue
 }
 
-export const nameAZ: SortingFunction<HasName> = (item1: HasName, item2: HasName): number => {
+export function nameAZ(item1: HasName, item2: HasName): number {
   return item1.name.localeCompare(item2.name) || byUrn(item1, item2)
 }
 
-export const nameZA: SortingFunction<HasName> = (item1: HasName, item2: HasName): number => {
+export function nameZA(item1: HasName, item2: HasName): number {
   return item2.name.localeCompare(item1.name) || byUrn(item1, item2)
 }
 
-export const rarest: SortingFunction<HasRarity> = (item1: HasRarity, item2: HasRarity): number => {
+export function rarest(item1: HasRarity, item2: HasRarity): number {
   return compareByRarity(item1, item2) || byUrn(item1, item2)
 }
 
-export const leastRare: SortingFunction<HasRarity> = (item1: HasRarity, item2: HasRarity): number => {
+export function leastRare(item1: HasRarity, item2: HasRarity): number {
   return compareByRarity(item2, item1) || byUrn(item1, item2)
 }
 
-export const newest: SortingFunction<HasDate> = (item1: HasDate, item2: HasDate): number => {
+export function newest(item1: HasDate, item2: HasDate): number {
   return item2.maxTransferredAt - item1.maxTransferredAt || byUrn(item1, item2)
 }
 
-export const oldest: SortingFunction<HasDate> = (item1: HasDate, item2: HasDate): number => {
+export function oldest(item1: HasDate, item2: HasDate): number {
   return item1.minTransferredAt - item2.minTransferredAt || byUrn(item2, item1)
 }
 
@@ -43,10 +45,7 @@ function hasRarity(item: Partial<HasRarity>): item is HasRarity {
   return !!item.rarity
 }
 
-export const rarestOptional: SortingFunction<HasUrn & Partial<HasRarity>> = (
-  item1: HasUrn & Partial<HasRarity>,
-  item2: HasUrn & Partial<HasRarity>
-): number => {
+export function rarestOptional(item1: HasUrn & Partial<HasRarity>, item2: HasUrn & Partial<HasRarity>): number {
   if (hasRarity(item1) && hasRarity(item2)) {
     return compareByRarity(item1, item2) || byUrn(item1, item2)
   } else if (!hasRarity(item1) && !hasRarity(item2)) {
@@ -58,10 +57,7 @@ export const rarestOptional: SortingFunction<HasUrn & Partial<HasRarity>> = (
   }
 }
 
-export const leastRareOptional: SortingFunction<HasUrn & Partial<HasRarity>> = (
-  item1: HasUrn & Partial<HasRarity>,
-  item2: HasUrn & Partial<HasRarity>
-): number => {
+export function leastRareOptional(item1: HasUrn & Partial<HasRarity>, item2: HasUrn & Partial<HasRarity>): number {
   return -rarestOptional(item1, item2)
 }
 
@@ -69,10 +65,7 @@ function hasDate(item: Partial<HasDate>): item is HasDate {
   return !!item.maxTransferredAt && !!item.minTransferredAt
 }
 
-export const newestOptional: SortingFunction<HasUrn & Partial<HasDate>> = (
-  item1: HasUrn & Partial<HasDate>,
-  item2: HasUrn & Partial<HasDate>
-): number => {
+export function newestOptional(item1: HasUrn & Partial<HasDate>, item2: HasUrn & Partial<HasDate>): number {
   if (hasDate(item1) && hasDate(item2)) {
     return item2.maxTransferredAt - item1.maxTransferredAt || byUrn(item1, item2)
   } else if (!hasDate(item1) && !hasDate(item2)) {
@@ -84,10 +77,7 @@ export const newestOptional: SortingFunction<HasUrn & Partial<HasDate>> = (
   }
 }
 
-export const oldestOptional: SortingFunction<HasUrn & Partial<HasDate>> = (
-  item1: HasUrn & Partial<HasDate>,
-  item2: HasUrn & Partial<HasDate>
-): number => {
+export function oldestOptional(item1: HasUrn & Partial<HasDate>, item2: HasUrn & Partial<HasDate>): number {
   if (hasDate(item1) && hasDate(item2)) {
     return item1.minTransferredAt - item2.minTransferredAt || byUrn(item2, item1)
   } else if (!hasDate(item1) && !hasDate(item2)) {
