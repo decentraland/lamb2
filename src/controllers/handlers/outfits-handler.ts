46ec9d0696fd71d6a8d4ec7c59ffbe96b2f40bb8
Pedro Tamborindeguy <pftambo@gmail.com>
2023-05-30 10:30:01 -0300

    chore: stuff

--- src/controllers/handlers/outfits-handler.ts
+++ src/controllers/handlers/outfits-handler.ts
@@ -1,13 +1,13 @@
-import { HandlerContextWithPath, NotFoundError, TypedEntity } from '../../types'
+import { HandlerContextWithPath, NotFoundError } from '../../types'
 import { getOutfits } from '../../logic/outfits'
-import { Outfits } from '@dcl/schemas'
+import { Entity } from '@dcl/schemas'
 
 export async function outfitsHandler(
   context: HandlerContextWithPath<
     'metrics' | 'content' | 'theGraph' | 'config' | 'fetch' | 'ownershipCaches',
     '/profile/:id'
   >
-): Promise<{ status: 200; body: TypedEntity<Outfits> }> {
+): Promise<{ status: 200; body: Entity }> {
   const outfits = await getOutfits(context.components, `${context.params.id}:outfits`)
   if (!outfits) {
     throw new NotFoundError('Outfits not found')
