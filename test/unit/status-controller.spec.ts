30f8d98d2c1711e1f072cd2873590a4119113bf1
Hugo Arregui <hugo@decentraland.org>
2023-05-31 15:53:18 -0300

    Save Wed May 31 03:53:18 PM -03 2023

--- test/unit/status-controller.spec.ts
+++ test/unit/status-controller.spec.ts
@@ -1,22 +1,22 @@
-import { createDotEnvConfigComponent } from '@well-known-components/env-config-provider'
+import { createConfigComponent, createDotEnvConfigComponent } from '@well-known-components/env-config-provider'
 import { statusHandler } from '../../src/controllers/handlers/status-handler'
 
 describe('status-controller-unit', () => {
   it('must return commit hash', async () => {
     const url = new URL('https://github.com/well-known-components')
-    const config = await createDotEnvConfigComponent({}, { COMMIT_HASH: 'commit_hash' })
+    const config = createConfigComponent({ COMMIT_HASH: 'commit_hash' })
     expect(await statusHandler({ url, components: { config } })).toMatchObject({ body: { commitHash: 'commit_hash' } })
   })
 
   it('must return current version', async () => {
     const url = new URL('https://github.com/well-known-components')
-    const config = await createDotEnvConfigComponent({}, { CURRENT_VERSION: 'current_version' })
+    const config = createConfigComponent({ CURRENT_VERSION: 'current_version' })
     expect(await statusHandler({ url, components: { config } })).toMatchObject({ body: { version: 'current_version' } })
   })
 
   it('must return currentTime', async () => {
     const url = new URL('https://github.com/well-known-components')
-    const config = await createDotEnvConfigComponent({}, {})
+    const config = createConfigComponent({})
     expect(await statusHandler({ url, components: { config } })).toMatchObject({
       body: { currentTime: expect.any(Number) }
     })
@@ -24,7 +24,7 @@ describe('status-controller-unit', () => {
 
   it('must return empty for values that does not have default value', async () => {
     const url = new URL('https://github.com/well-known-components')
-    const config = await createDotEnvConfigComponent({}, {})
+    const config = createConfigComponent({})
     expect(await statusHandler({ url, components: { config } })).toMatchObject({
       body: { commitHash: '', version: '' }
     })
