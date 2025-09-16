ad81f32634bea6dfa5e3c84c272e3392953d56b3
Braian Mellor <braianj@gmail.com>
2025-09-16 14:29:54 -0300

    feat: add const 24 hours

--- src/adapters/entities-fetcher.ts
+++ src/adapters/entities-fetcher.ts
@@ -31,6 +31,7 @@ export type EntitiesFetcher = IBaseComponent & {
 }
 
 const MAX_COLLECTION_PAGE_SIZE = 1000
+const TWENTY_FOUR_HOURS_IN_MS = 24 * 60 * 60 * 1000
 
 export async function createEntitiesFetcherComponent({
   config,
@@ -43,7 +44,7 @@ export async function createEntitiesFetcherComponent({
   const itemsAge = (await config.getNumber('ITEMS_CACHE_MAX_AGE')) ?? 600000 // 10 minutes by default
 
   const collectionCacheSize = (await config.getNumber('COLLECTION_CACHE_MAX_SIZE')) ?? 20
-  const collectionCacheAge = (await config.getNumber('COLLECTION_CACHE_MAX_AGE')) ?? 86400000 // 24 hours
+  const collectionCacheAge = (await config.getNumber('COLLECTION_CACHE_MAX_AGE')) ?? TWENTY_FOUR_HOURS_IN_MS // 24 hours
 
   const entititesCache = createLowerCaseKeysCache<Entity>({ max: itemsSize, ttl: itemsAge })
   const collectionsCache = createLowerCaseKeysCache<CollectionCacheData>({
