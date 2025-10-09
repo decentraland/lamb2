a52206d9ca37a2d0ccbb66df33e7eadfac202a86
Andres Morelos <6563162+AndresMorelos@users.noreply.github.com>
2025-10-09 11:16:03 -0500

    refactor: remove isSmartWearable filter from createFilters function since we are not getting off-chan and third-party wearables, and the on-chain are filtered at theGraph

--- src/controllers/handlers/items-commons.ts
+++ src/controllers/handlers/items-commons.ts
@@ -9,24 +9,12 @@ export type FilterableItem = {
   entity?: Entity
 }
 
-function isSmartWearable(item: FilterableItem): boolean {
-  if (!item.entity) {
-    return false
-  }
-
-  return (
-    item.entity.type === 'wearable' && item.entity.content?.some((content) => content.file.endsWith('.js')) === true
-  )
-}
-
 export function createFilters(url: URL): (item: FilterableItem) => boolean {
   const categories = url.searchParams.has('category')
     ? url.searchParams.getAll('category').map((category) => category.toLowerCase())
     : []
   const name = url.searchParams.has('name') ? url.searchParams.get('name')!.toLowerCase() : undefined
   const rarity = url.searchParams.has('rarity') ? url.searchParams.get('rarity')!.toLowerCase() : undefined
-  const isSmartWearablesParam = url.searchParams.get('isSmartWearable')
-  const filterSmartWearable = isSmartWearablesParam === 'true' || isSmartWearablesParam === '1'
 
   if (rarity && !SORTED_RARITIES.includes(rarity as Rarity)) {
     throw new InvalidRequestError(`Invalid rarity requested: '${rarity}'.`)
@@ -42,9 +30,7 @@ export function createFilters(url: URL): (item: FilterableItem) => boolean {
     if (categories && categories.length > 0 && (!item.category || !categories.includes(item.category))) {
       return false
     }
-    if (filterSmartWearable && !isSmartWearable(item)) {
-      return false
-    }
+
     return true
   }
 }
